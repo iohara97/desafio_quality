534b18806fa8ff859a96dc62f6e90ddcdc7070f5
VanessaGomesOliveira <vanessa.goliveira@mercadolivre.com>
2022-01-05 14:43:51 -0300

    Teste unitario 4 e 5

--- src/test/java/br/com/meli/spring3/entityTest/CasaTest.java
+++ src/test/java/br/com/meli/spring3/entityTest/CasaTest.java
@@ -14,8 +14,15 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * Classe para testar os métodos da Entity Casa
+ */
 public class CasaTest {
 
+    /**
+     * Método que deve confirmar se o método calcular área está correto
+     * @throws IOException
+     */
     @Test
     public void deveRetornarMetrosQuadrados() throws IOException {
 
@@ -33,35 +40,29 @@ public class CasaTest {
         assert(casa.calculaArea() == 210);
     }
 
+    /**
+     * Método que confirma se o método maiorComodo está correto
+     * @throws IOException
+     */
     @Test
-    public void testaBairroQueNaoExiste() {
+    public void verificarMaiorComodo() throws IOException {
 
-        BairroRepository mock = Mockito.mock(BairroRepository.class);
-        BairroRepository bairroRepository = new BairroRepository(mock);
+        List<Comodo> comodos = new ArrayList<>();
+        comodos.add(new Comodo("cozinha", 10, 15));
+        comodos.add(new Comodo("sala", 5, 12));
+        Comodo comodoMaior = new Comodo("quarto1", 15, 12);
+        comodos.add(comodoMaior);
+        comodos.add(new Comodo("quarto2", 5, 12));
 
-        String bairro = "Para";
+        Casa casa = Casa.builder()
+                .nome("casa")
+                .endereco("Bairro da Flor")
+                .valorMetroQuadrado(new BigDecimal(100))
+                .comodos(comodos)
+                .build();
 
-        // intera com bairros do repository
-        //assert(bairroRepository.bairroExiste(bairro));
+        assert(casa.maiorComodo() == comodoMaior);
 
-        BusinessException excecaoEsperada = assertThrows(
-                BusinessException.class,
-                () -> bairroRepository.bairroExiste(bairro) //act
-        );
-
-        assertTrue(excecaoEsperada.getMessage().contains("Bairro não encontrado."));
     }
-
-    @Test
-    public void testaBairroQueExiste() {
-
-        BairroRepository mock = Mockito.mock(BairroRepository.class);
-        BairroRepository bairroRepository = new BairroRepository(mock);
-
-        String bairro = "Belenzinho";
-
-        assertTrue(bairroRepository.bairroExiste(bairro));
-    }
-
 
 }
